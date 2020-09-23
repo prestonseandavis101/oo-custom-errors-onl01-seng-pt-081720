8970d2e3f187844992852ae4c01c98d638bb03d4
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-23 03:41:06 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,8 +7,13 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != Person 
-      raise PartnerError
+    if person.class != Person
+      begin
+        raise PartnerError
+        rescue PartnerError
+        puts
+        error.message
+      end
     else
     person.partner = self
   end
